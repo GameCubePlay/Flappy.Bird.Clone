2def8c7c59d2929d3070e94675c6d02149bfcfb2
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-05-17 20:57:10 +0200

    Initialise and sprite loading

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Bird implements Updatable, Renderable{
 
@@ -6,6 +7,20 @@ public class Bird implements Updatable, Renderable{
     private float yVel;
     private float baseYVel = -6.0f;
     private float gravity = 0.25f;
+
+    private Pipes pipes;
+    private int scoredPipe = 0;
+    private int score = 0;
+    private Font gameFont = new Font("Arial", Font.BOLD, 30);
+
+    private BufferedImage flapUp;
+    private BufferedImage flapDown;
+
+    public Bird(Pipes pipes) {
+        resetBird();
+
+        this.pipes = pipes;
+    }
 
     public void resetBird(){
         x = 100;
